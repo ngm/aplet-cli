1d245c62195b44abe779cf0814e766f9fa061564
Neil Mather <ngmather@gmail.com>
2017-12-24 18:58:42 +0000

    Initial version of init and makedocs commands

--- aplet.py
+++ aplet.py
@@ -1,6 +1,21 @@
+from os import listdir, path, makedirs, chdir
 import click
-import os
+import parsefm
 import shutil
+import subprocess
+import utilities
+import yaml
+
+config = {}
+
+def load_config(filename):
+    with open(filename, "r") as stream:
+        try:
+            global config
+            config = yaml.load(stream)
+        except yaml.YAMLError as ex:
+            print(ex)
+
 
 @click.group()
 def aplet():
@@ -9,13 +24,95 @@ def aplet():
 
 @aplet.command()
 @click.option("--projectfolder", default=".", help="Location to output the aplet files")
-def init(**kwargs):
-    os.mkdir("productline")
-    shutil.copyfile("templates/model.xml", os.path.join(kwargs['projectfolder'], "productline/model.xml"))
-    os.mkdir("productline/configs")
-    shutil.copyfile("templates/ExampleProduct.config", os.path.join(kwargs['projectfolder'], "productline/configs/ExampleProduct.config"))
+def init(projectfolder):
+    productline_dir = path.join(projectfolder, "productline")
+    configs_path = path.join(productline_dir, "configs")
+    bddfeatures_path = path.join(projectfolder, "bddfeatures")
+    testreports_path = path.join(projectfolder, "testreports")
+
+    if not path.exists(productline_dir):
+        makedirs(productline_dir)
+    shutil.copyfile("templates/model.xml", path.join(productline_dir, "model.xml"))
+
+    if not path.exists(configs_path):
+        makedirs(configs_path)
+    shutil.copyfile("templates/ExampleProduct.config", path.join(configs_path, "ExampleProduct.config"))
+
+    if not path.exists(bddfeatures_path):
+        makedirs(bddfeatures_path)
+
+    if not path.exists(testreports_path):
+        makedirs(testreports_path)
+
+
+
+@aplet.command()
+@click.option("--projectfolder", default=".", help="Location to output the aplet files")
+@click.option("--runtests/--no-runtests", default=False)
+def makedocs(projectfolder, runtests):
+    featuremodel_path = path.join(projectfolder, "productline", "model.xml")
+    configs_path = path.join(projectfolder, "productline", "configs")
+    bddfeatures_path = path.join(projectfolder, "bddfeatures")
+    testreports_path = path.join(projectfolder, "testreports")
+
+    docs_dir = path.join(projectfolder, "docs")
+    if not path.exists(docs_dir):
+        makedirs(docs_dir)
+
+    lektor_temp_dir = path.join(docs_dir, "lektor")
+    if not path.exists(lektor_temp_dir):
+        shutil.copytree("templates/lektor", lektor_temp_dir)
+    print(config)
+    utilities.sed_inplace(path.join(lektor_temp_dir, "aplet.lektorproject"), r'<<PROJECT>>', config["project_name"])
+
+    # TODO: don't hardcode product names
+    products = [path.splitext(product_path)[0] for product_path in listdir(configs_path)]
+    for product in products:
+        #cp eclipse/configs/$PRODUCT.config $APP_DIR/todo.config
+        #export FEATURES=$(sed 's?\(.*\)?-g \1 ?' eclipse/configs/$PRODUCT.config | tr -d '\r\n')
+        #export NOTFEATURES=$(comm -13 <(sort eclipse/configs/$PRODUCT.config) <(sort eclipse/configs/optionals.config) | sed 's?\(.*\)?-g Not\1 ?' | tr -d '\r\n')
+
+        if runtests:
+            click.echo("Running tests")
+            #php vendor/bin/codecept run acceptance $FEATURES $NOTFEATURES --debug --json --html --xml
+            # copying report file for product
+            #cp tests/_output/report.json tests/_output/reports/report$PRODUCT.json
+            #cp tests/_output/report.xml tests/_output/reports/report$PRODUCT.xml
+            #cp tests/_output/report.html tests/_output/reports/report$PRODUCT.html
+
+        current_product_lektor_dir = path.join(lektor_temp_dir, "content/products", product)
+        if not path.exists(current_product_lektor_dir):
+            makedirs(current_product_lektor_dir)
+
+        product_filepath = path.join(current_product_lektor_dir,"contents.lr")
+        shutil.copyfile("templates/lektor/helpers/product_contents.lr", product_filepath)
+
+        utilities.sed_inplace(product_filepath, r'<<PRODUCT>>', product)
+
+        if runtests:
+            click.echo("Running tests")
+            #cp tests/_output/report.html build/lektor/content/products/$PRODUCT/report$PRODUCT.html
+
+        #python3 scripts/product_has_failed.py tests/_output/reports/ $PRODUCT
+        #if [ $? -eq 0 ]; then
+        #    sed -i "s/<<PASS_STATUS>>/true/g" build/lektor/content/products/$PRODUCT/contents.lr;
+        #else
+        #    sed -i "s/<<PASS_STATUS>>/false/g" build/lektor/content/products/$PRODUCT/contents.lr;
+        #fi
+        product_config_file = path.join(projectfolder, "productline/configs", product + ".config")
+
+        parsefm.parse_feature_model(featuremodel_path, bddfeatures_path, testreports_path, product_config_file, current_product_lektor_dir, "feature_model")
+
+    click.echo("- Generating feature model SVG...")
+    click.echo(featuremodel_path)
+    parsefm.parse_feature_model(featuremodel_path, bddfeatures_path, testreports_path, "all", path.join(lektor_temp_dir, "content/"), "feature_model")
+
+    click.echo("- Building site")
+    chdir(lektor_temp_dir)
+    subprocess.call(["lektor", "build", "-O" "out"]) 
 
 
 if __name__ == '__main__':
+    load_config("aplet.yml")
     aplet()
 
---
+++ parsefm.py
@@ -0,0 +1,140 @@
+from gherkin3.parser import Parser
+import xml.etree.ElementTree as et
+import graphviz as gv
+from os import listdir, path, makedirs
+import json
+import pprint
+import hashlib
+import pprint
+
+gherkin_parser = Parser()
+tags = {}
+
+
+def parse_feature(feature, parent, graph, test_results, product_features):
+    feature_name = feature.get("name")
+    feature_is_abstract = feature.get("abstract") is not None
+
+    has_failed_test = False
+    fillcolor = "white"
+
+    if product_features and feature_name not in product_features and not feature_is_abstract:
+        return has_failed_test and False
+
+    for child in feature.getchildren():
+        child_has_failed = parse_feature(child, feature, graph, test_results, product_features)
+        has_failed_test = has_failed_test or child_has_failed
+
+    # add gherkin nodes
+    if feature_name in tags:
+        tags_for_feature = tags[feature_name]
+        with graph.subgraph(name="cluster_" + feature_name) as subgraph:
+            feature_name_hash = hashlib.sha256(feature_name.encode('utf-8')).hexdigest()
+            subgraph_root_name = "cluster_" + feature_name_hash
+            subgraph.node(subgraph_root_name, label="", shape="none", width="0", height="0", style="invis")
+            graph.edge(feature_name, subgraph_root_name)
+            subgraph.attr(rankdir="TB")
+            previous = subgraph_root_name
+            for piece_name in tags_for_feature:
+                piece_hash = hashlib.sha256(piece_name.encode('utf-8')).hexdigest()
+                line_color = "#000000"
+                if piece_name[3:] in test_results:
+                    if test_results[piece_name[3:]] == True:
+                        line_color = "#00cc00"
+                    elif test_results[piece_name[3:]] == False:
+                        line_color = "#ff0000"
+                        has_failed_test = True
+                subgraph.node(piece_hash, piece_name, color=line_color, fillcolor="white", shape="box")
+                subgraph.edge(previous, piece_hash, style="invis", weight="0")
+                previous = piece_hash
+
+    # add fmfeature node
+    line_color = "green"
+    if feature_is_abstract:
+        fillcolor = "#cccccc"
+        if has_failed_test is True:
+            fillcolor = "#ffcccc"
+            line_color = "red"
+    else:
+        if has_failed_test is True:
+            fillcolor = "#ffeeee"
+            line_color = "red"
+
+    graph.node(feature_name, feature_name, fillcolor=fillcolor, style='filled', shape='box', color=line_color)
+
+    # add edge to parent
+    if parent is not None:
+        parent_name = parent.get("name")
+        arrowhead = "odot"
+        if feature.get("mandatory") is not None:
+            arrowhead = "dot"
+        graph.edge(parent_name, feature_name, arrowhead=arrowhead)
+
+    return has_failed_test
+
+def parse_feature_model(model_xml_file, features_dir, reports_dir, productconfig, output_dir, output_filename):
+    for feature_file in listdir(features_dir):
+        feature_file = open(path.join(features_dir, feature_file), "r")
+        feature_parsed = gherkin_parser.parse(feature_file.read())
+
+        for tag in feature_parsed['tags']:
+            tag_name = tag['name'][1:] # remove @
+            if tag_name not in tags:
+                tags[tag_name] = []
+            tags[tag_name].append("F: " + feature_parsed['name'])
+
+        for scenario in feature_parsed['scenarioDefinitions']:
+            for tag in scenario['tags']:
+                tag_name = tag['name'][1:] # remove @
+                if tag_name not in tags:
+                    tags[tag_name] = []
+                tags[tag_name].append("S: " + scenario['name'])
+
+    # Parse tests results
+    # For all product reports, go through results
+    # If there's a failure in any product for a given feature, that's a failure for the PL.
+    pl_test_results = {}
+    xml_files = [file for file in listdir(reports_dir) if file.endswith(".xml")]
+    for test_results_file in xml_files:
+        file_path = path.join(reports_dir, test_results_file)
+        tree = et.parse(file_path)
+        root = tree.getroot()
+        acceptance_suite = root.find('testsuite')
+
+        for testcase in acceptance_suite:
+            scenario_name = testcase.get("feature")
+            passed = True
+            if testcase.find("failure") is not None:
+                passed = False
+            if scenario_name not in pl_test_results:
+                pl_test_results[scenario_name] = True
+            pl_test_results[scenario_name] = pl_test_results[scenario_name] and passed
+
+
+    # Parse feature model
+    tree = et.parse(model_xml_file)
+    root = tree.getroot()
+    features_root = root.find('struct')
+    graph = gv.Digraph(format="svg")
+
+
+    # prefilter by features for product if we have a product flag
+    product_features = []
+    if productconfig is not "all":
+        if not path.exists(productconfig):
+            raise IOError("File {0} does not exist".format(productconfig))
+        # features = features filtered by product config
+        with open(productconfig) as product_config_file:
+            for config_option in product_config_file:
+                print(config_option.strip())
+                product_features.append(config_option.strip())
+
+        product_features.append("todoapp")
+
+    for feature in features_root.getchildren():
+        parse_feature(feature, None, graph, pl_test_results, product_features)
+
+    graph.render(filename=path.join(output_dir, output_filename))
+
+if __name__ == '__main__':
+    parse_feature_model()
